50c3f8cb6241406c956d1ba1fc9ede7f77941fd0
nilay <mailnilaydas@gmail.com>
2020-09-14 10:01:29 +0530

    1st commit

---
+++ src/main/java/com/example/thymeleafemployeedemo/ThymeleafEmployeeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafemployeedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafEmployeeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafEmployeeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleafemployeedemo/ThymeleafEmployeeDemoApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.example.thymeleafemployeedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafEmployeeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
